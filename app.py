741b99c370db3063a26c9e4effbb130f342e6c0b
VedhaSankar <vedhasankar26@gmail.com>
2022-07-28 21:24:46 +0530

    Updates for 21:24:46   28/07/22

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
 from flask import Flask, render_template
 import datetime
+import logging
+import random
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.WARNING, format = 'Warning Structure Fomrat')
+
 
 @app.route('/')
 def start():
 
     return render_template('index.html')
+
+@app.route('/log')
+def log():
+
+    # generate random number between 0 and 31423413
+    x = random.randint(0, 31423413)
+
+    if x % 2 == 0:
+
+        logging.warning('Log Message')
+
+    elif x % 3 == 0:
+
+        logging.critical('Log Message')
+
+    else:
+
+        logging.error('Log Message')
+
+    return render_template('index.html')
+
 
 
 @app.route('/time')
